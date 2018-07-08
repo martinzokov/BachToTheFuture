30873c39b219165cf9a43211c0b99653a1c6bc86
mzokov <mzokov@users.noreply.github.com>
2018-07-08 19:04:40 +0100

    Updated train.py for Python 3

--- train.py
+++ train.py
@@ -14,27 +14,27 @@ settings = DataHandler.get_config_params()
 
 data_handler = DataHandler(t_steps=settings["t_steps"], t_step_length=settings["t_step_length"])
 
-print 'Loading data:'
+print('Loading data:')
 for midi_file in os.listdir(settings["data_dir"]):
-    print 'processing:', midi_file
+    print('processing:', midi_file)
     notes = data_handler.get_np_notes(settings["data_dir"] + midi_file)
     training_samples.append(notes)
-    print notes[0].shape
+    print(notes[0].shape)
     if len(notes[0]) == 0:
-        print 'delete ', midi_file
-        print len(notes[0].shape), len(notes[1])
+        print('delete ', midi_file)
+        print(len(notes[0].shape), len(notes[1]))
 
-print 'Data loaded'
+print('Data loaded')
 neurons = settings["neurons"]
 dropout = settings["dropout"]
 l_rate = settings["learning_rate"]
 epochs = settings["epochs"]
 optimizer = settings["optimizer"]
 file_name = str(neurons) + "n_" + str(dropout) + "do_" + str(l_rate) + "lr_" + str(epochs) + "epochs_" + \
-            str(len(training_samples)) + "samps_"+optimizer+".hdf5"
+            str(len(training_samples)) + "samps_"+optimizer+".h5"
 
-print 'Building model:'
+print('Building model:')
 model = Model(neurons=neurons, dropout=dropout, learning_rate=l_rate, optimizer=optimizer, desired_loss=0.2)
 
-print 'Training:'
+print('Training:')
 model.train_model(training_samples, epochs=epochs, save_weigths=True, save_file=settings["weights_dir"] + file_name)
